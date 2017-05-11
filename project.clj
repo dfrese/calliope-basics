7e46501c028952b8932e9cd2034ec8ddb4714b95
David Frese <david.frese@active-group.de>
2017-05-11 23:51:25 +0200

    Add a project description.

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject dfrese/calliope-basics "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "Basic utilities to write calliope application."
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
